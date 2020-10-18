a0a775828c68644bb2bb8401c968378bb597b1e8
Livia Qian <qianlivia@gmail.com>
2020-10-18 22:50:10 +0200

    Combined the two occlusion experiments

--- src/experiments/occlusion/1_4_occlusion.py
+++ src/experiments/occlusion/1_4_occlusion.py
@@ -18,7 +18,7 @@ from ast import literal_eval
 # score and patches that have high heatmap values for Grad-CAM and Guided Grad-CAM on the ILSVRC-12 val
 # dataset. Rank correlation should be averaged over 2510 images.
 
-def calculate_rank_correlation(model, df, layer, guided_gradcam=False, plot=False, use_pred=False):
+def calculate_rank_correlation(model, df, layer, plot=False, use_pred=False):
     # Get device (so you can use gpu if possible)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
@@ -32,21 +32,23 @@ def calculate_rank_correlation(model, df, layer, guided_gradcam=False, plot=Fals
 
     # Run Grad-CAM
     gcm = gradCAM(model, layer)
-    if guided_gradcam:
-        gbp = guidedBackProp(model)
+    # Run Guided Backprop
+    gbp = guidedBackProp(model)
     
     it = 0  # we will use to keep track of the batch location
-    scores = []
-
+    grad_cam_scores = []
+    guided_grad_cam_scores = []
+    
     # Loop through batches
     for batch_images in tqdm(validationLoader):
         batch_images = batch_images.to(device)
 
         # df_view gives us access the dataframe on the batch window
         df_view = df.iloc[it:min(it + batch_size, df_count_size)]
+        
+        # Forward pass
         gcm.forward(batch_images)
-        if guided_gradcam:
-           gbp.forward(batch_images)
+        gbp.forward(batch_images)
 
         if use_pred: # Use top-1 prediction as label
             batch_prob, batch_label = gcm.getTopK(k=1)
@@ -57,42 +59,59 @@ def calculate_rank_correlation(model, df, layer, guided_gradcam=False, plot=Fals
             batch_prob = gcm.probs[:, true_label]
             batch_label = torch.Tensor([true_label]).to(torch.int64).to(device)
         
-        # generate the heatmaps
+        # Generate the heatmaps
+        # Grad-CAM
         mapCam = gcm.generateMapClassBatch(batch_label)
         heatmaps = gcm.generateCam(mapCam, layer[0], image_path=None, mergeWithImage=False, isBatch=True)
-        if guided_gradcam:
-            mapGuidedBackProp = gbp.generateMapClassBatch(batch_label)
-            gradientNumpy = gradientToImageBatch(mapGuidedBackProp)
-            heatmaps = heatmaps * gradientNumpy
+        # Guided Grad-CAM
+        mapGuidedBackProp = gbp.generateMapClassBatch(batch_label)
+        gradientNumpy = gradientToImageBatch(mapGuidedBackProp)
+        heatmaps_guided = heatmaps * gradientNumpy
             
         # Generate the occlusion maps
         with torch.no_grad():
             occlusion_maps = occlusion(model, batch_images, batch_label, batch_prob, invert=True)
 
         for i in range(batch_size):  # iterate over batch
-            hm = heatmaps[i]
+            # Fetch maps corresponding to current iteration
+            grad_cam_hm = heatmaps[i]
+            guided_grad_cam_hm = heatmaps_guided[i]
             occlusion_map = occlusion_maps[i]
                 
-            # Convert heatmap to grayscale image if it's 3D
-            if guided_gradcam:
-                hm = np.dot(hm[..., :3], [0.2989, 0.5870, 0.1140])
+            # Convert heatmap to grayscale image (Guided Grad-CAM)
+            guided_grad_cam_hm = np.dot(guided_grad_cam_hm[..., :3], [0.2989, 0.5870, 0.1140])
             
             # Normalize the occlusion map
             occlusion_map = occlusion_map - occlusion_map.min()
             occlusion_map = occlusion_map / occlusion_map.max()
 
-            # Calculate rank correlation between heatmap and occlusion map
-            hm_rank = get_rank(hm)
+            # Calculate rank correlation between each heatmap and the occlusion map
+            # Get ranks
+            grad_cam_hm_rank = get_rank(grad_cam_hm)
+            guided_grad_cam_hm_rank = get_rank(guided_grad_cam_hm)
             occlusion_map_rank = get_rank(occlusion_map)
-            coeff, p = spearmanr(hm_rank, occlusion_map_rank)
-            scores.append(coeff)
-            print(coeff)
+            
+            # Grad-CAM heatmap
+            grad_cam_coeff, grad_cam_p = spearmanr(grad_cam_hm_rank, occlusion_map_rank)
+            grad_cam_scores.append(grad_cam_coeff)
+            
+            # Guided Grad-CAM heatmap
+            guided_grad_cam_coeff, guided_grad_cam_p = spearmanr(guided_grad_cam_hm_rank, occlusion_map_rank)
+            guided_grad_cam_scores.append(guided_grad_cam_coeff)
+            print(grad_cam_coeff, guided_grad_cam_coeff)
             
             # Plot images
             if plot:
-                ngc.gradCAM.plot_heatmap(batch_images[i], hm)
+                # Grad-CAM heatmap
+                ngc.gradCAM.plot_heatmap(batch_images[i], grad_cam_hm)
                 plt.show()
                 
+                # Guided Grad-CAM heatmap
+                plt.imshow(guided_grad_cam_hm, cmap=plt.get_cmap('gray'))
+                plt.colorbar()
+                plt.show()
+                
+                # Occlusion map
                 plt.imshow(occlusion_map, cmap=plt.cm.bwr_r, vmin=0, vmax=1)
                 plt.colorbar()
                 plt.show()
@@ -100,7 +119,7 @@ def calculate_rank_correlation(model, df, layer, guided_gradcam=False, plot=Fals
         it += batch_size
         
     # Return a list of hits and misses + max activations
-    return np.array(scores)
+    return np.array(grad_cam_scores), np.array(guided_grad_cam_scores)
 
 def get_rank(map):
     return np.argsort(map.flatten())
@@ -131,10 +150,12 @@ def occlusion(model, images, label, prob, invert=True):
                 image[:, :, i:(i + block_h), j:(j + block_w)] = mean
                 images_to_process.append(image)
                 
+                # If the desired number of images have been collected or this is the last iteration 
                 if (it + 1) % batch_s == 0 or i == h - 1 and j == w - 1:
                     # Forward pass
                     images_to_process = torch.cat(images_to_process, dim=0)
                     
+                    # Collect prediction
                     pred = F.softmax(model(images_to_process), dim=1)
                     pred = (pred[:, int(label[n])]).detach().cpu().numpy().tolist()
                     batch_prob.extend(pred)
@@ -157,9 +178,8 @@ def occlusion(model, images, label, prob, invert=True):
 
 df = pd.read_csv("../../../datasets/res2_2510.csv")
 print("Running Grad-CAM on VGG16 and calculating rank correlation with occlusion maps...")
-scores = calculate_rank_correlation(getVGGModel(16), layer=['features.29'], df=df, guided_gradcam=False, plot=False, use_pred=False)
-print("Average score: ", np.average(scores))
-
-print("Running Guided Grad-CAM on VGG16 and calculating rank correlation with occlusion maps...")
-scores = calculate_rank_correlation(getVGGModel(16), layer=['features.29'], df=df, guided_gradcam=True, plot=False, use_pred=False)
-print("Average score: ", np.average(scores))
+grad_cam_scores, guided_grad_cam_scores = calculate_rank_correlation(getVGGModel(16), layer=['features.29'], df=df, plot=False, use_pred=False)
+np.savetxt("result_gradcam.csv", grad_cam_scores, delimiter=",")
+np.savetxt("result_guided_gradcam.csv", guided_grad_cam_scores, delimiter=",")
+print("[Grad-CAM] Average score: ", np.average(grad_cam_scores))
+print("[Guided Grad-CAM] Average score: ", np.average(guided_grad_cam_scores))
